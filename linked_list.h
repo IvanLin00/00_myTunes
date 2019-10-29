f07d6b0ddf0db95b9f78529ac07921d9afc27b77
Ivan Lin <ilin00@stuy.edu>
2019-10-29 11:44:49 -0400

    v1

---
+++ linked_list.h
@@ -0,0 +1,14 @@
+struct song_node{
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
+
+struct song_node * insert_front(struct song_node *n, char name[100], char artist[100]);
+struct song_node * order(struct song_node *n, char name[100], char artist[100]);
+void print_list(struct song_node *n);
+struct song_node * find(struct song_node *n,char name[100], char artist[100]);
+char * find_artist(struct song_node *n, char artist[100]);
+struct song_node * random_element(struct song_node *n);
+struct song_node * remove(struct song_node *n, char name[100], char artist[100]);
+struct song_node * free(struct song_node *n);
