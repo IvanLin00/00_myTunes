ca45a38f6d77de2d0539f784e74a5e8af05700a4
JimmyLuo0 <jluo00@stuy.edu>
2019-10-29 16:53:59 -0400

    idk

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "linked_list.h"
+
+void print_list(struct song_node *n){
+  printf("[ ");
+  while(n != NULL){
+    printf("%s : %s, ", n->artist, n->name );
+    n = n->next;
+  }
+  printf("]\n");
+}
+
+struct song_node * insert_front(struct song_node *n, char name[100], char artist[100]){
+  struct song_node *insert = malloc(sizeof(struct song_node));
+  strncpy(insert->name, name, 100);
+  strncpy(insert->artist, artist, 100);
+  insert->next = n;
+  return insert;
+}
+
+//wip
+struct song_node * find(struct song_node *n,char name[100], char artist[100]){
+  struct song_node *tofind = n;
+  if(n->name == name && n->artist == artist)
+    return n;
+  //while(tofind->name != name && tofind->artist != artist)    seg fault
+    //tofind = tofind->next;
+  return tofind;
+}
--- main.c
+++ main.c
@@ -1,3 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "linked_list.h"
+
+int main(){
+  struct song_node *songs = NULL;
+  print_list(songs);
+
+  songs = insert_front(songs, "snow halation", "muse");
+  songs = insert_front(songs, "padoru padoru", "umu");
+  songs = insert_front(songs, "d/dx(x^2)", "kill me");
+  print_list(songs);
+
+  find(songs, "d/dx(x^2)", "kill me");
+  return 0;
+}
