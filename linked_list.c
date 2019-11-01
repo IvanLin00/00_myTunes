022beda4c8aaf0d4f1852b07aadedaf980bca24b
Ivan Lin <ilin00@stuy.edu>
2019-10-31 21:02:44 -0400

    functions to test + new files

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,4 @@
+#include <rand.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,12 +12,11 @@ void print_list(struct song_node *n){
   }
   printf("]\n");
 }
-
-// void print_node(struct song_node *n){
-//   struct song_node *toprint = n;
-//   toprint->next = NULL;
-//   print_list(toprint);
-// }
+//need to test
+void print_node(struct song_node *n){
+  struct song_node *toprint = n;
+  printf("[%s : %s", n->artist, n->name);
+}
 
 struct song_node * insert_front(struct song_node *n, char name[100], char artist[100]){
   struct song_node *insert = malloc(sizeof(struct song_node));
@@ -25,8 +25,28 @@ struct song_node * insert_front(struct song_node *n, char name[100], char artist
   insert->next = n;
   return insert;
 }
+//need to test
+struct song_node * order(struct song_node *n, char name[100], char artist[100]){
+	struct song_node *insert = malloc(sizeof(struct song_node));
+	strncpy(insert->name, name, 100);
+	strncpy(insert->artist, artist, 100);
+	struct song_node front = n;
+	if(strcmp(front->artist,insert->artist) < 0 && strcmp(front->name,insert->name) < 0){
+		insert -> next = n;
+		return insert;
+	}
+	while(front != NULL){
+		if(strcmp(front->artist,insert->artist) < 0){
+			if(strcmp(front->name,insert->name) < 0){
+				break;
+			}
+		}
+		front = front -> next;
+	}
+	front -> next = insert;
+	return front;
+}
 
-//wip
 struct song_node * find(struct song_node *n,char name[100], char artist[100]){
   struct song_node *tofind = n;
   while(tofind != NULL){
@@ -36,7 +56,27 @@ struct song_node * find(struct song_node *n,char name[100], char artist[100]){
      tofind = tofind-> next;
    }
 }
-
+//need to test
+struct song_node * find(struct song_node *n, char artist[100]){
+	struct song_node *tofind = n;
+	while(tofind != NULL){
+		if(strcmp(tofind->artist, artist) == 0){
+		   return tofind;
+		 }
+		 tofind = tofind-> next;
+	}
+}
+//need to test
+struct song_node * random_element(struct song_node *n, int num_nodes);{
+	int node_index = num_nodes * rand();
+	struct song_node *random_node = n;
+	while (node_index > 0 && random_node != NULL){
+		random_node = random_node -> next;
+		node_index -= 1;
+	}
+	return random_node;
+}
+//wip
 struct song_node * remove_node(struct song_node *n, char name[100], char artist[100]){\
   struct song_node *findn = find(n,name,artist);
   if(findn = n){
--- linked_list.h
+++ linked_list.h
@@ -8,7 +8,7 @@ struct song_node * insert_front(struct song_node *n, char name[100], char artist
 struct song_node * order(struct song_node *n, char name[100], char artist[100]);
 void print_list(struct song_node *n);
 struct song_node * find(struct song_node *n,char name[100], char artist[100]);
-char * find_artist(struct song_node *n, char artist[100]);
-struct song_node * random_element(struct song_node *n);
+struct song_node * find_artist(struct song_node *n, char artist[100]);
+struct song_node * random_element(struct song_node *n, int num_nodes);
 struct song_node * remove_node(struct song_node *n, char name[100], char artist[100]);
 struct song_node * free_list(struct song_node *n);
